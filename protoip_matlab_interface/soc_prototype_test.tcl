b87569c7c76c9865a15cac63e017bbf0959ed96e
Bulat Khusainov <bulat_khusainov@mail.ru>
2016-08-02 12:46:15 +0100

    don't call soc_prototype_test in for PL template

--- protoip_matlab_interface/soc_prototype_test.tcl
+++ protoip_matlab_interface/soc_prototype_test.tcl
@@ -1,5 +1,6 @@
 
 #load function arguments passed from Matlab
+set error 0
 set  file_name ""
 append file_name ".metadata/configuration_parameters_matlab_interface.dat"
 set fp [open $file_name r]
@@ -108,6 +109,11 @@ set type_design_flow "matlab"
 set type_template $old_type_template
 
 #added by Bulat
+if {!($type_template == "SOC")} {
+	error " -E- This function can be used only for SOC template. Consider using ip_prototype_test function or change to SOC template"ex
+	incr error
+}
+
 set num_soc_input_vectors [lindex $data [expr [lsearch $data "#soc_Input"] + 1 ]]
 set soc_input_vectors {}
 set soc_input_vectors_length {}
@@ -127,63 +133,66 @@ set soc_output_vectors_length {}
 	}
 #end added by Bulat
 
-set tmp_dir "soc_prototype/test/results/"
-append tmp_dir $r_project_name
-file mkdir $tmp_dir
+# condition is added by Bulat
+if {$error==0} {
 
-set type_design_flow "matlab"
-[tclapp::icl::protoip::make_template::make_project_configuration_parameters_dat $r_project_name $input_vectors $input_vectors_length $input_vectors_type $input_vectors_integer_length $input_vectors_fraction_length $output_vectors $output_vectors_length $output_vectors_type $output_vectors_integer_length $output_vectors_fraction_length $fclk $FPGA_name $board_name $type_eth $mem_base_address $num_test $type_test $type_template $type_design_flow $soc_input_vectors $soc_input_vectors_length $soc_output_vectors $soc_output_vectors_length]
-[::tclapp::icl::protoip::make_template::make_ip_configuration_parameters_readme_txt $r_project_name]
-# update ip_design/src/FPGAclientAPI.h file
-[::tclapp::icl::protoip::make_template::make_soc_FPGAclientAPI_h  $r_project_name]
+	set tmp_dir "soc_prototype/test/results/"
+	append tmp_dir $r_project_name
+	file mkdir $tmp_dir
 
-set tmp_dir "soc_prototype/test/results/"
-append tmp_dir $r_project_name
-cd $tmp_dir
+	set type_design_flow "matlab"
+	[tclapp::icl::protoip::make_template::make_project_configuration_parameters_dat $r_project_name $input_vectors $input_vectors_length $input_vectors_type $input_vectors_integer_length $input_vectors_fraction_length $output_vectors $output_vectors_length $output_vectors_type $output_vectors_integer_length $output_vectors_fraction_length $fclk $FPGA_name $board_name $type_eth $mem_base_address $num_test $type_test $type_template $type_design_flow $soc_input_vectors $soc_input_vectors_length $soc_output_vectors $soc_output_vectors_length]
+	[::tclapp::icl::protoip::make_template::make_ip_configuration_parameters_readme_txt $r_project_name]
+	# update ip_design/src/FPGAclientAPI.h file
+	[::tclapp::icl::protoip::make_template::make_soc_FPGAclientAPI_h  $r_project_name]
 
-set time_stamp [clock format [clock seconds] -format "%Y-%m-%d_T%H-%M"]
+	set tmp_dir "soc_prototype/test/results/"
+	append tmp_dir $r_project_name
+	cd $tmp_dir
 
-foreach i $soc_input_vectors {
+	set time_stamp [clock format [clock seconds] -format "%Y-%m-%d_T%H-%M"]
+
+	foreach i $soc_input_vectors {
+		set file_name ""
+		append file_name "soc_" $i "_in_log.dat"
+		if {[file exists $file_name] == 1} { 
+			set file_name_new ""
+			append file_name_new $time_stamp "_backup_soc_" $i "_in_log.dat"
+			file copy -force $file_name $file_name_new
+			file delete -force $file_name
+		}
+	}
+
+	foreach i $soc_output_vectors {
+		set file_name ""
+		append file_name "fpga_soc_" $i "_out_log.dat"
+		if {[file exists $file_name] == 1} { 
+			set file_name_new ""
+			append file_name_new $time_stamp "_backup_fpga_soc_" $i "_out_log.dat"
+			file copy -force $file_name $file_name_new
+			file delete -force $file_name
+		}
+		#set file_name ""
+		#append file_name "matlab_" $i "_out_log.dat"
+		#if {[file exists $file_name] == 1} { 
+		#	set file_name_new ""
+		#	append file_name_new $time_stamp "_backup_matlab_" $i "_out_log.dat"
+		#	file copy -force $file_name $file_name_new
+		#	file delete -force $file_name
+		#}
+	}
+
 	set file_name ""
-	append file_name "soc_" $i "_in_log.dat"
+	append file_name "fpga_time_log.dat"
 	if {[file exists $file_name] == 1} { 
 		set file_name_new ""
-		append file_name_new $time_stamp "_backup_soc_" $i "_in_log.dat"
+		append file_name_new $time_stamp "_backup_fpga_time_log.dat"
 		file copy -force $file_name $file_name_new
 		file delete -force $file_name
 	}
-}
 
-foreach i $soc_output_vectors {
-	set file_name ""
-	append file_name "fpga_soc_" $i "_out_log.dat"
-	if {[file exists $file_name] == 1} { 
-		set file_name_new ""
-		append file_name_new $time_stamp "_backup_fpga_soc_" $i "_out_log.dat"
-		file copy -force $file_name $file_name_new
-		file delete -force $file_name
-	}
-	#set file_name ""
-	#append file_name "matlab_" $i "_out_log.dat"
-	#if {[file exists $file_name] == 1} { 
-	#	set file_name_new ""
-	#	append file_name_new $time_stamp "_backup_matlab_" $i "_out_log.dat"
-	#	file copy -force $file_name $file_name_new
-	#	file delete -force $file_name
-	#}
+	cd ../../../../
 }
-
-set file_name ""
-append file_name "fpga_time_log.dat"
-if {[file exists $file_name] == 1} { 
-	set file_name_new ""
-	append file_name_new $time_stamp "_backup_fpga_time_log.dat"
-	file copy -force $file_name $file_name_new
-	file delete -force $file_name
-}
-
-cd ../../../../
-	
 
 
 exit
